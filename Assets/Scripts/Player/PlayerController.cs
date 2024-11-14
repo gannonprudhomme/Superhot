e3146603808a8acb9ddb90638df0bb4193ffd699
Gannon Prudhomme <gannonprudhomme@gmail.com>
2024-11-13 19:09:09 -0800

    Added jumping

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,94 +1,30 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.Serialization;
 
 #nullable enable
 
 [RequireComponent(
-    typeof(CharacterController),
+    typeof(PlayerMovementController),
     typeof(Target)
 )]
 public class PlayerController : MonoBehaviour {
     [Header("References")]
     public Pistol? EquippedWeapon; // this will be private & handled by picking up weapons later
     
-    [Tooltip("The player / first-person camera")]
-    public Camera? Camera;
-    
-    private InputAction? moveAction;
-    private InputAction? lookAction;
     private InputAction? fireAction;
-    private CharacterController? characterController;
-    public float maxSpeed = 5f;
-    public float movementSharpness = 10f;
-    private Vector3 velocity = Vector3.zero;
+    private InputAction? throwAction;
 
-    private const float minTimeDilation = 0.1f;
-    public float TimeDilation {
-        get {
-            float currSpeed = velocity.magnitude;
-            
-            // Prevent it from going below minTimeDilation
-            float dilation = Mathf.Max(currSpeed / maxSpeed, minTimeDilation);
-            
-            // Prevent it from being above 1
-            return Mathf.Min(dilation, 1f);
-        }
-    }
-    
     private void Awake() {
-        characterController = GetComponent<CharacterController>();
-
-        moveAction = InputSystem.actions.FindAction("Move");
-        lookAction = InputSystem.actions.FindAction("Look");
+        // TODO: Remove this duh
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        
         fireAction = InputSystem.actions.FindAction("Attack");
+        throwAction = InputSystem.actions.FindAction("Throw");
     }
 
     private void Update() {
-        HandleMovement();
-        HandleCameraMovement();
         HandleInputs();
-
-        Time.timeScale = TimeDilation;
-    }
-
-    private void HandleMovement() {
-        // Get horizontal / vertical input
-        Vector2 movement = moveAction!.ReadValue<Vector2>();
-
-        // Move the player
-        Vector3 moveDirection = new Vector3(movement.x, 0, movement.y);
-        
-        Vector3 worldSpaceMove = transform.TransformVector(moveDirection);
-        Vector3 targetVelocity = worldSpaceMove * maxSpeed;
-
-        velocity = Vector3.Lerp(
-            velocity,
-            targetVelocity,
-            movementSharpness * Time.deltaTime
-        );
-        
-        characterController!.Move(velocity * Time.deltaTime);
-    }
-
-    // Angles per second? Idk
-    public float RotationSpeed = 0.1f;
-
-    private float verticalCameraAngle = 0f;
-    private void HandleCameraMovement() {
-        
-        Vector2 look = lookAction!.ReadValue<Vector2>();
-
-        float horizontalRotation = (look.x) * RotationSpeed;
-        transform.Rotate(new Vector3(0, horizontalRotation, 0), Space.Self);
-
-        float invertedVerticalLook = -look.y;
-        float verticalMouseMovement = invertedVerticalLook * RotationSpeed;
-        verticalCameraAngle += verticalMouseMovement;
-        
-        verticalCameraAngle = Mathf.Clamp(verticalCameraAngle, -89f, 90f);
-        
-        Camera!.transform.localEulerAngles = new(verticalCameraAngle, 0, 0);
     }
 
     private void HandleInputs() {
@@ -99,11 +35,5 @@ public class PlayerController : MonoBehaviour {
         if (fireAction!.WasReleasedThisFrame()) {
             EquippedWeapon!.FireReleased();
         }
-    }
-    
-    // Gets a reoriented direction that is tanget to a given slope
-    private static Vector3 GetDirectionReorientedOnSlope(Vector3 direction, Vector3 slopeNormal, Vector3 transformUp) {
-        Vector3 directionRight = Vector3.Cross(direction, transformUp);
-        return Vector3.Cross(slopeNormal, directionRight).normalized;
     }
 }
---
+++ Assets/Scripts/Player/PlayerMovementController.cs
@@ -0,0 +1,160 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+#nullable enable
+
+[RequireComponent(
+    typeof(CharacterController)
+)]
+public class PlayerMovementController : MonoBehaviour  {
+    [Header("References")]
+    [Tooltip("The player / first-person camera")]
+    public Camera? Camera;
+    
+    [Header("Values")]
+    public float RotationSpeed = 0.1f;
+    
+    private InputAction? moveAction;
+    private InputAction? jumpAction;
+    private InputAction? lookAction;
+    private CharacterController? characterController;
+    
+    private const float MaxSpeed = 10f;
+    private const float MovementSharpness = 5f;
+    
+    private float verticalCameraAngle = 0f;
+    private Vector3 velocity = Vector3.zero;
+
+    private const float DownForce = 55f;
+    private const float TerminalVelocity = -10f;
+    private const float JumpForce = 20f;
+
+    public float TimeDilation {
+        get {
+            // When we're in the air it's always 1
+            // (note the time dilation logic doesn't work when it's in the air, but it's fine cause we do this)
+            if (!characterController!.isGrounded) {
+                return 1f;
+            }
+            
+            float currSpeed = velocity.magnitude;
+            
+            // Prevent it from going below minTimeDilation
+            const float minTimeDilation = 0.1f;
+            float dilation = Mathf.Max(currSpeed / MaxSpeed, minTimeDilation);
+            
+            // Prevent it from being above 1
+            return Mathf.Min(dilation, 1f);
+        }
+    }
+
+    private void Awake() {
+        characterController = GetComponent<CharacterController>();
+
+        moveAction = InputSystem.actions.FindAction("Move");
+        lookAction = InputSystem.actions.FindAction("Look");
+        jumpAction = InputSystem.actions.FindAction("Jump");
+    }
+
+    // Update is called once per frame
+    private void Update() {
+        HandleMovement();
+        HandleCameraMovement();
+        
+        Time.timeScale = TimeDilation;
+    }
+
+    private void HandleMovement() {
+        // Get horizontal / vertical input
+        var movement = moveAction!.ReadValue<Vector2>();
+
+        // Move the player
+        Vector3 moveDirection = new(movement.x, 0, movement.y);
+        
+        Vector3 worldSpaceMove = transform.TransformVector(moveDirection);
+        
+        // TODO: Handle when in the air (I think there's a jump?)
+
+        if (characterController!.isGrounded) {
+            Vector3 targetVelocity = worldSpaceMove * MaxSpeed;
+
+            velocity = Vector3.Lerp(
+                velocity,
+                targetVelocity,
+                MovementSharpness * Time.deltaTime
+            );
+        } else {
+            Vector3 targetVelocity = worldSpaceMove * MaxSpeed;
+
+            // Treat only horizontal movement like we do with ground movement
+            float x = Mathf.Lerp(
+                velocity.x,
+                targetVelocity.x,
+                MovementSharpness * Time.unscaledDeltaTime
+            );
+
+            float z = Mathf.Lerp(
+                velocity.z,
+                targetVelocity.z,
+                MovementSharpness * Time.unscaledDeltaTime
+            );
+
+            // Handle vertical velocity (isn't limited like horizontal is)
+
+            // Note this is negative as it's a downward force
+            float y = velocity.y + (-DownForce * Time.deltaTime); // note this is actually subtracting
+
+            // Clamp it to the terminal velocity
+            y = Mathf.Max(y, TerminalVelocity);
+
+            velocity = new Vector3(x, y, z);
+        }
+
+        if (HandleJump(velocity, out Vector3 newVelocity)) {
+            Debug.Log("Jumping!");
+            velocity = newVelocity;
+        }
+
+        characterController!.Move(velocity * Time.deltaTime);
+    }
+    
+    // Angles per second? Idk
+
+    private void HandleCameraMovement() {
+        Vector2 look = lookAction!.ReadValue<Vector2>();
+
+        float horizontalRotation = (look.x) * RotationSpeed;
+        transform.Rotate(new Vector3(0, horizontalRotation, 0), Space.Self);
+
+        float invertedVerticalLook = -look.y;
+        float verticalMouseMovement = invertedVerticalLook * RotationSpeed;
+        verticalCameraAngle += verticalMouseMovement;
+        
+        verticalCameraAngle = Mathf.Clamp(verticalCameraAngle, -89f, 90f);
+        
+        Camera!.transform.localEulerAngles = new(verticalCameraAngle, 0, 0);
+    }
+    
+    private bool HandleJump(Vector3 inputVelocity, out Vector3 newVelocity) {
+        if (!characterController!.isGrounded) {
+            newVelocity = Vector3.zero;
+            return false;
+        }
+
+        if (!jumpAction!.WasPressedThisFrame()) {
+            newVelocity = Vector3.zero;
+            return false;
+        }
+        
+        newVelocity = inputVelocity;
+        newVelocity.y = JumpForce;
+        
+        return true;
+    }
+    
+    // Gets a reoriented direction that is tangent to a given slope
+    private static Vector3 GetDirectionReorientedOnSlope(Vector3 direction, Vector3 slopeNormal, Vector3 transformUp) {
+        Vector3 directionRight = Vector3.Cross(direction, transformUp);
+        return Vector3.Cross(slopeNormal, directionRight).normalized;
+    }
+}
